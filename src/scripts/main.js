73cbd398ba6d677d40f03a6fb79a508acd7cd9ef
berkeley platte <berkeleyplatte989@gmail.com>
2019-06-10 10:43:33 -0500

    followed along coding with teacher

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,5 @@
 function cl(data) {
-    console.log(data);
+  console.log(data);
 }
 
-fetch("http://localhost:8088/food")
-.then ( data => data.json())
-.then ( foodData => {
-    cl(foodData);
-})
+getMyFood()
